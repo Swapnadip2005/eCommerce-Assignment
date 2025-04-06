a50bbee5011ebddaeceaaa6b4efef2812305c3cc
Swapnadip2005 <swapnadippaulofficial@gmail.com>
2025-04-06 21:45:18 +0530

    Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import { Provider } from 'react-redux';
+import store from '@/context/Store';
+import '@/styles/globals.css';
+import Navbar from '@/components/Navbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default function App(props) {
+  return (
+    <Provider store={store}>
+      <MyApp {...props} />
+    </Provider>
+  );
 }
